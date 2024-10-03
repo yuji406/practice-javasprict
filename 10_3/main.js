4b894b772257820aea12ba7e6dfb4381e6436d41
yuji406 <yuji.200108@gmail.com>
2024-10-03 05:20:27 +0000

    10_3

---
+++ 10_3/main.js
@@ -0,0 +1,10 @@
+function errorInput(){
+    console.log(document.getElementById("month").value)
+    console.log(document.getElementById("day").value)
+
+    const month = document.getElementById("month").value
+    const month = document.getElementById("day").value
+
+console.log ("month:",month,"day",day)
+
+}
