5dfbe421fb27efadc4f86b2789eb16fcccbd6dc6
yuji406 <yuji.200108@gmail.com>
2024-10-04 06:17:48 +0000

    10_4

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ function errorInput(){
 console.log ("month:",month,"day",day)
 
 }
+
+const monthSelect = document.getElementById("month2")
+monthSelectlist()
+function monthSelectlist(){
+    for(let i=1;i<=12;i++){
+        let optionn = document .createElement('option')
+        optionn.value=i+"月"
+        monthSelect.appendChild('option')
+    }
+}
