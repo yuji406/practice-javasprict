f2348147d47a93ca57258e48e0a86b3ca9bd2c72
yuji406 <yuji.200108@gmail.com>
2025-01-10 05:47:10 +0000

    関数の勉強

---
+++ main.js
@@ -0,0 +1,19 @@
+function ClickcounterGame(){
+    let count = 0;
+    const gameContainer = document.getElementById("game-container")
+
+let button1 = document.createElement("button");
+button1.textContent = "+1ボタン";
+
+let counter = document.createElement("p");
+counter.textContent = "count"; 
+
+button1.addEventListener("click",function(){
+    count++;
+    counter.textContent = count;
+})
+
+gameContainer.appendChild(button1);
+gameContainer.appendChild(counter);
+} 
+ClickcounterGame();
