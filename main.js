cc65997e5588f29c32462aad4331837bebde0cf7
yuji406 <yuji.200108@gmail.com>
2025-01-30 05:14:22 +0000

    カウントの追加

--- main.js
+++ main.js
@@ -67,7 +67,14 @@ function startNumberGuessGame(){
         }else{
             message.textContent = "小さい"
         }
+        count++;
+        countDisplay.textContent = `試行回数: ${count}`;
     })
     gameContainer.appendChild(button);
-}
+
+    let count = 0;
+    const countDisplay = document.createElement("p");
+    countDisplay.textContent = `試行回数: ${count}`;
+    gameContainer.appendChild(countDisplay);
+}  
 startNumberGuessGame();
